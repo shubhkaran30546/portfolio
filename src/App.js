8eb14428e8b49d5a591c28f2132c5ef9e4f1f9f6
shubhkaran30546 <73439286+shubhkaran30546@users.noreply.github.com>
2024-07-02 17:09:52 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
 import Contact from './components/Contact';
 import About from './components/About';
 import Projects from './components/Projects';
-// import About from './components/About'; // Assuming you have an About component
 
 function App() {
   return (
-    <body>
-    <Router>
-      <Navbar title="Home" className="navbar"/>
-      <Routes>
-        <Route path="/" element={<Profile />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />}/>
-      </Routes>
-      <div className="bottom-line"></div>
-    </Router>
-    </body>
+    <div>
+      <Router basename="/portfolio">
+        <Navbar title="Home" className="navbar" />
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Profile />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+        <div className="bottom-line"></div>
+      </Router>
+    </div>
   );
 }
 
