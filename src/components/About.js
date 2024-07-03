88dfa74a0616d377a782a45dfeaa1ff534bbc0eb
shubhkaran30546 <73439286+shubhkaran30546@users.noreply.github.com>
2024-07-03 01:07:25 -0600

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,6 @@
 import React, { memo } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGraduationCap, faSchool } from '@fortawesome/free-solid-svg-icons';
-import { FaGraduationCap } from 'react-icons/fa';
 
 export default function About() {
   return (
